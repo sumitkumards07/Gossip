cd0bd38813da3efc73e52fdaf4d4858eb6357ddc
sumitkumards07 <sumitkumards07@gmail.com>
2025-12-31 00:21:14 +0530

    fix: update bot name to riya

--- server/index.js
+++ server/index.js
@@ -180,8 +180,8 @@ Behavior:
 `;
 
                 const BOTS = [
-                    { id: 'BOT_RIYA', name: 'riya_sharma', prompt: `You are Riya from Mumbai.${COMMON_INSTRUCTIONS} Talk about Mumbai traffic, rains, marine drive, vada pav.` },
-                    { id: 'BOT_ZARA', name: 'zara_kapoor', prompt: `You are Zara from South Delhi.${COMMON_INSTRUCTIONS} You are slightly rich / sassy.Use words like "Hawww", "Achaaaa".Talk about momos, shopping, cafes.` },
+                    { id: 'BOT_RIYA', name: 'riya', prompt: `You are Riya from Mumbai.${COMMON_INSTRUCTIONS} Talk about Mumbai traffic, rains, marine drive, vada pav.` },
+                    { id: 'BOT_ZARA', name: 'zara', prompt: `You are Zara from South Delhi.${COMMON_INSTRUCTIONS} You are slightly rich / sassy.Use words like "Hawww", "Achaaaa".Talk about momos, shopping, cafes.` },
                     { id: 'BOT_ANANYA', name: 'anu_xyz', prompt: `You are Ananya from Bangalore.${COMMON_INSTRUCTIONS} You are an IT girl / sweet.Talk about weather, bengaluru traffic, startups, coffee.` },
                     { id: 'BOT_PRIYA', name: 'priya_04', prompt: `You are Priya from Pune.${COMMON_INSTRUCTIONS} You are a gamer / chill.Use "bro", "scene", "chill maar".Talk about gaming, pune weather, treks.` }
                 ];
